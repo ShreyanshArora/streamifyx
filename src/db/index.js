2838ba3910f82a8e8db7839645a2b098ce237c1e
Shreyansh Arora <shreyansharora69@gmail.com>
2024-10-24 00:48:16 +0530

    handlers and controllers configured

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`mongodb://0.0.0.0:27017/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection error: ", error);
